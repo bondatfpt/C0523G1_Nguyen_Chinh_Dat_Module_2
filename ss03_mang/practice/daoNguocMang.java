72ad26dafe6b6e4d1513b843e951678806fe3ea3
bondatfpt <bondatfpt@gmail.com>
2023-07-05 21:31:00 +0700

    Chinh Dat nop bai tap

--- ss03_mang/practice/daoNguocMang.java
+++ ss03_mang/practice/daoNguocMang.java
@@ -2,7 +2,7 @@ package ss03_mang.practice;
 
 import java.util.Scanner;
 
-public class timMax {
+public class daoNguocMang {
     public static void main(String[] args) {
         int size;
         int[] array;
@@ -11,28 +11,27 @@ public class timMax {
             System.out.print("Enter a size:");
             size = scanner.nextInt();
             if (size > 20)
-                System.out.println("Size should not exceed 20");
+                System.out.println("Size does not exceed 20");
         } while (size > 20);
         array = new int[size];
         int i = 0;
         while (i < array.length) {
-            System.out.print("Enter element" + (i + 1) + " : ");
+            System.out.print("Enter element " + (i + 1) + ": ");
             array[i] = scanner.nextInt();
             i++;
         }
-        System.out.print("Property list: ");
+        System.out.printf("%-20s%s", "Elements in array: ", "");
         for (int j = 0; j < array.length; j++) {
             System.out.print(array[j] + "\t");
         }
-        int max = array[0];
-        int index = 1;
-        for (int j = 0; j < array.length; j++) {
-            if (array[j] > max) {
-                max = array[j];
-                index = j + 1;
-            }
+        for (int j = 0; j < array.length / 2; j++) {
+            int temp = array[j];
+            array[j] = array[size - 1 - j];
+            array[size - 1 - j] = temp;
         }
-        System.out.println("The largest property value in the list is " + max + " ,at position " + index);
-
+        System.out.printf("\n%-20s%s", "Reverse array: ", "");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+        }
     }
 }

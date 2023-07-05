e3769628d675d898dadffe287f3a33ed63e71f68
bondatfpt <bondatfpt@gmail.com>
2023-07-05 20:19:18 +0700

    Chinh Dat nop bai tap

---
+++ ss03_mang/exercise/timMinTrongMang.java
@@ -0,0 +1,28 @@
+package ss03_mang.exercise;
+
+import java.util.Scanner;
+
+public class timMinTrongMang {
+    // Tương tự bài tìm max nhưng là mảng 1 chiều;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhap vao so luong phan tu cua mang");
+        int amount = input.nextInt();
+        int array[] = new int[amount];
+        for (int i = 0; i < amount; i++) {
+            System.out.println("Nhap vao gia tri phan tu thu " + (i + 1) );
+            array[i] = input.nextInt();
+        }
+        for (int i = 0; i < amount; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("\n");
+        int min = array[0];
+        for (int i = 0; i < amount; i++) {
+            if (array[i] < min){
+                min = array[i];
+            }
+        }
+        System.out.println("Phan tu co gia tri nho nhat la: " + min);
+    }
+}
---
+++ ss03_mang/exercise/tinhTongTrongMang.java
@@ -0,0 +1,10 @@
+package ss03_mang.exercise;
+
+import java.util.Scanner;
+
+public class tinhTongTrongMang {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+    }
+}

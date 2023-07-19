42a5b9218ad90d25eb3874b3e751bbe127eae52d
bondatfpt <bondatfpt@gmail.com>
2023-07-19 17:13:42 +0700

    Chinh Dat nop bai tap

---
+++ ss13_search_algorithm/practice/exe1/BinarySearch.java
@@ -0,0 +1,26 @@
+package ss13_search_algorithm.practice.exe1;
+
+public class BinarySearch {
+    static int list [] = {2,4,5,7,9,10,20,40,45,49,56,60,62,72};
+    public static void main(String[] args) {
+        System.out.println(binarySearch(72,list));
+        System.out.println(list.length);
+    }
+    public  static int binarySearch(int key, int list[]){
+        int low = 0;
+        int high = list.length - 1;
+        while (high >= low){
+            int mid = (low + high) / 2;
+            if(key < list[mid]){
+                high = mid - 1;
+            } else if (key == list[mid]) {
+                return mid;
+            }else {
+                low = mid + 1 ;
+            }
+        }
+        return -1;
+    }
+
+}
+

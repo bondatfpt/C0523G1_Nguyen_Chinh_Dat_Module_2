b13a1585de907c6da6a2a38eb76b8b19302798c3
bondatfpt <bondatfpt@gmail.com>
2023-07-27 19:11:33 +0700

    Chinh Dat nop bai tap

--- ss18_string_and_regex/exercise/exe3/CrawlNews.java
+++ ss18_string_and_regex/exercise/exe3/CrawlNews.java
@@ -1,5 +1,42 @@
 package ss18_string_and_regex.exercise.exe3;
 
-public class CrawlNews {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class CrawlNews {
+    private static final String REGEX_NEWS_LIST = "<h3.*?</h3>";
+    public static void main(String[] args) throws IOException {
+        //Lấy data HTML từ https://dantri.com.vn/the-gioi.htm
+        URL link = new URL("https://dantri.com.vn/the-gioi.htm");
+        URLConnection connection = link.openConnection();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuilder dataLine = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            dataLine.append(line);
+        }
+        //Gộp những chuỗi theo regex "<h .* <h/3>"
+        Pattern pattern = Pattern.compile(REGEX_NEWS_LIST);
+        Matcher matcher = pattern.matcher(dataLine);
+        ArrayList<String> newsList = new ArrayList<>();
+        while (matcher.find()) {
+            newsList.add(matcher.group());
+        }
+        String[] temp;
+        List<String> resultList = new ArrayList<>();
+        for (String str : newsList) {
+            temp = str.split("htm\">");
+            resultList.add(temp[1].substring(0, temp[1].length() - 9));
+        }
+        for (String str : resultList) {
+            System.out.println(str.replaceAll("&quot;", "\"").trim());
+        }
+    }
 }

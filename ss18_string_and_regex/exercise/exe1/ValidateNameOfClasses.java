fa9a15d7c5d5e70b5a4c2609434db921aff10a89
bondatfpt <bondatfpt@gmail.com>
2023-07-27 18:49:37 +0700

    Chinh Dat nop bai tap

---
+++ ss18_string_and_regex/exercise/exe1/ValidateNameOfClasses.java
@@ -0,0 +1,19 @@
+package ss18_string_and_regex.exercise.exe1;
+
+import java.util.Scanner;
+
+public class ValidateNameOfClasses {
+    private final static String REGEX_NAME_OF_CLASS = "^[CAP][0-9]{4}[GHIK]$";
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a name class");
+        String nameClasses = input.nextLine();
+        if(nameClasses.matches(REGEX_NAME_OF_CLASS)){
+            System.out.println("Valid name class");
+        }
+        else {
+            System.out.println("Invalid name class!");
+        }
+    }
+}
---
+++ ss18_string_and_regex/exercise/exe2/ValidatePhoneNumber.java
@@ -0,0 +1,26 @@
+package ss18_string_and_regex.exercise.exe2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ValidatePhoneNumber {
+    private final static String REGEX_PHONE_NUMBER = "^[(]\\d{2}[)]-[(]0\\d{9}[)]$";
+
+    public static void main(String[] args) {
+        List<String> listPhoneNumber = new ArrayList<>();
+        listPhoneNumber.add("(84)-(0978489648)");
+        listPhoneNumber.add("(a8)-(22222222)");
+        listPhoneNumber.add("(84)-(22b22222)");
+        listPhoneNumber.add("(84)-(9978489648)");
+        for (String phoneNumber:listPhoneNumber) {
+            if(phoneNumber.matches(REGEX_PHONE_NUMBER)){
+                System.out.println("Valid phone number: " +phoneNumber);
+            }
+            else {
+                System.out.println("Invalid phone number: " +phoneNumber);
+            }
+        }
+
+    }
+}

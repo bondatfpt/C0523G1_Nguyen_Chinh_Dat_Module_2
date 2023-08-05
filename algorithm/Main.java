a4cb78b6c1b609100fab08a718d20dbc0de44e20
bondatfpt <bondatfpt@gmail.com>
2023-08-06 00:05:17 +0700

    Chinh Dat nop bai tap

--- algorithm/Main.java
+++ algorithm/Main.java
@@ -8,8 +8,34 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class Main {
-    public static void main(String[] sgr) {
-     String string = "SVRO-0001";
-        System.out.println(string = string.substring(5,string.length()));
+   public static String chuanHoaChuoi (String string){
+       string = string.trim().toLowerCase();
+       String result = "";
+       int countSpace = 0;
+       boolean checkKyTuDau = true;
+       String arr [] = string.split("");
+       for (int i = 0; i < arr.length; i++) {
+           if(arr[i].equals(" ")){
+               countSpace++;
+               checkKyTuDau = true;
+           }else {
+               if(checkKyTuDau){
+                   result = result.concat(arr[i].toUpperCase());
+               }
+               else {
+                   result = result.concat(arr[i]);
+               }
+               checkKyTuDau = false;
+               countSpace = 0;
+           }
+           if(countSpace == 1){
+               result = result.concat(" ");
+           }
+       }
+       return result;
+   }
+
+    public static void main(String[] args) {
+        System.out.println(chuanHoaChuoi("   ngUYEN     cHinh    dAt"));
     }
 }
